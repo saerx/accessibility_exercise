f32141415f97edba42460fb13be5c2da408fe26d
saerx <saerlaith.robyn@gmail.com>
2021-06-06 20:32:29 +0100

    initialised asite and added placeholder text and favicon

---
+++ src/containers/MainContentContainer.js
@@ -0,0 +1,19 @@
+const MainContentContainer = () => {
+
+    return (
+    <>
+        <h1>Accessibility Exercise</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac hendrerit leo. Donec a erat tincidunt magna lobortis pulvinar. Mauris accumsan sed nibh eget elementum. Cras aliquam mollis velit. Pellentesque sit amet pharetra metus. Pellentesque pretium efficitur magna, at suscipit orci consequat eu. Sed cursus tristique laoreet. Aliquam elementum sagittis efficitur.
+        </p>
+        <p>
+          Sed eleifend dui sed auctor accumsan. Praesent mollis enim nec sollicitudin pharetra. Proin convallis leo sit amet lorem vestibulum condimentum venenatis et libero. Integer tristique urna ut finibus scelerisque. Pellentesque lobortis, mi ac fringilla lacinia, odio purus hendrerit sem, in gravida metus justo at ipsum. Ut blandit lectus nunc. Aenean sed vehicula ipsum, sed lacinia diam. Integer imperdiet, lorem at aliquam tempus, sem enim interdum neque, non imperdiet neque mauris non ipsum. Vivamus tellus ex, ornare quis dui ut, condimentum aliquet nibh. Aliquam rutrum purus at luctus porta. Sed blandit mauris vitae erat aliquet, sit amet venenatis magna vulputate. Vestibulum interdum nulla at sapien tempus faucibus. Proin eu consequat lectus. Nunc pulvinar, sem ut pellentesque suscipit, mauris purus hendrerit magna, et placerat nunc ligula id sem. Nullam quis porttitor arcu, sed ultrices nibh.
+        </p>
+        <p>
+          Etiam malesuada odio libero, id accumsan ipsum feugiat ac. Praesent vel tempus diam. Vivamus eget nisi turpis. Nam euismod risus quis arcu fermentum, non interdum ante ultrices. Sed in cursus arcu. Nulla velit turpis, bibendum et congue sed, elementum non mi. Mauris et urna sed nibh iaculis fermentum.
+        </p>
+    </>
+    )
+}
+
+export default MainContentContainer;
